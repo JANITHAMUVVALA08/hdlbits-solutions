35e9990aaa031d9f081ccf97de6159a5892bd43c
JANITHA MUVVALA <janithamuvvala@gmail.com>
2026-04-18 16:02:47 +0530

    Implement top_module with conditional output assignment

---
+++ always_if.v
@@ -0,0 +1,19 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always   ); 
+    always@(*)begin
+        if(sel_b1==1'b1&&sel_b2==1'b1)begin
+            out_always=b;
+        end
+        else begin
+            out_always=a;
+        end
+    end
+    assign out_assign=out_always;
+
+endmodule
